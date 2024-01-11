8fa3167820086e84e9c143942f9bf4f1a6f2e805
akashmore02 <156194217+akashmore02@users.noreply.github.com>
2024-01-11 16:57:03 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";  //treats all the code as the new version
+
+//datatypes
+let name = "Ramesh" //string
+let age = 18  //number the size of no can go upto 2^54
+let loggedininput = true //boolean 
+let state;
+
+/* Datatypes
+Number
+BigInt
+String
+Boolean  True/False
+Null
+Undefined
+*/
+
+console.log(typeof name)
+console.log(typeof age);
+console.log(typeof loggedininput)
+console.log(typeof state)  //type for a undefined value is Undefined
+
+console.log(typeof null)  //type of null is object
+
