0d052ec433ce208cefda0482ea384b97638de7c7
akashmore02 <156194217+akashmore02@users.noreply.github.com>
2024-01-12 16:31:27 +0000

    done

---
+++ 01_basics/typeconversion.js
@@ -0,0 +1,22 @@
+//conversion of one data type into another data type
+
+let name = "Ramesh"
+let a = "1234"
+let age = 18  
+let number = 1
+let loggedininput = true
+let state;
+
+let convertedname = Number(name)
+let converteda = Number(a)
+let convertedage = String(age)
+let convertedloggedininput = Number(loggedininput)
+let convertednumber = Boolean(number)
+
+
+
+console.log(convertedname);
+console.log(converteda);
+console.log(convertedage);
+console.log(convertedloggedininput);
+console.log(convertednumber);
