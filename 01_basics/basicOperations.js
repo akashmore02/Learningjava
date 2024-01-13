75b4ea0022dc7eaf4bde8cb4676a009c0b0a4148
akashmore02 <156194217+akashmore02@users.noreply.github.com>
2024-01-13 09:06:42 +0000

    remaining

---
+++ 01_basics/basicOperations.js
@@ -0,0 +1,38 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//arithmatic operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+//console.log(4**4); provides power. for ex 2**3 indicates 2^3=8 |  4**4=256
+//console.log(2/3); 
+// console.log(2%3); provides remainder
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+//in case of only numbers like 1+3 or 34+23 + operator does addtion operation
+//but in case there is a string in the operation like "2" + 1 it does concatenation like 21
+// console.log("1" + 2); //here it concatenates 1 and 2 as 12
+// console.log(1 + "2");  //here also it concatenates 1 and 2 as 12
+// console.log("1" + 2 + 2); //here if there is a string at the start then the output becomes 122
+// console.log(1 + 2 + "2"); //here first addition of numbers 1 and 2 is done then it is concatenated with 2 so output becomes 32
+
+let num5 = "1" + 2
+console.log(typeof num5)
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
