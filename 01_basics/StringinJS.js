fdc3084483c2a341754963deea9bfccd733c781c
akashmore02 <156194217+akashmore02@users.noreply.github.com>
2024-01-16 15:58:25 +0000

    done

---
+++ 01_basics/StringinJS.js
@@ -0,0 +1,15 @@
+const Yr = 2024
+//console.log("Happy New"+" year "+ Yr)
+//Above method is not a efficient way by using +
+//Instead we can use
+console.log(`Happy Diwali ${Yr}`);
+
+
+//another way of declaring a string by using object
+
+const name = new String("EshwarVazirani")
+console.log(name[2])
+console.log(name.charAt(4))
+console.log(name.length)
+console.log(name.substring(0,4));
+console.log(name.indexOf("V"))
