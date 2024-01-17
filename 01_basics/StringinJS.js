ce8a50ae7d672e5d757c57d45e6df179d6166b79
akashmore02 <156194217+akashmore02@users.noreply.github.com>
2024-01-17 12:10:34 +0000

    string functions

--- 01_basics/StringinJS.js
+++ 01_basics/StringinJS.js
@@ -11,5 +11,28 @@ const name = new String("EshwarVazirani")
 console.log(name[2])
 console.log(name.charAt(4))
 console.log(name.length)
-console.log(name.substring(0,4));
+console.log(name.substring(0,4))
 console.log(name.indexOf("V"))
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(0,4))
+
+const newName = "    WuShanClan   "
+console.log(newName);
+console.log(newName.trim())    //removes blank spaces the string. Doesn't remove space in between the words
+console.log(newName.trimStart())  //removes blank spaces at the start of the string
+console.log(newName.trimEnd())   //removes blank spaces at the end of the string
+
+const url = "www.akash%20more.com"
+console.log(url.replace("20", "-"))  //replaces one value in the string with another
+
+console.log(url.includes("akash"))  //returns true if the asked value is included in the string
+console.log(url.includes("tukaram"))  //returns false if the asked value is not included in the string
+
+const Splitex = "Happy Diwali Bhava"
+const Split1 = Splitex.split(" ")
+console.log(Split1)
+
+
+
+
