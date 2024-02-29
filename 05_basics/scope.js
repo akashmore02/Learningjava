c6b832b9cc21c5cb43f93275b0d66d7e0e72fc13
akashmore02 <156194217+akashmore02@users.noreply.github.com>
2024-02-29 10:20:34 +0000

    done

--- 05_basics/scope.js
+++ 05_basics/scope.js
@@ -15,7 +15,7 @@ function UserDetails()
 console.log(username)  //here the value of username will be Manish not akash because Username at the start is an global variable
 //So anything that is defined within the {} can't be used outside of that scope
 
-
+/*
 //scope in if statement
 if(true)      //if true is written then it wil execute the if block anyhow
 {    
@@ -27,3 +27,17 @@ console.log(name)
 }
 console.log(sirname)    // sirname wont will execute. It will give error as not defined as the scope of sirname was limited to above {} 
 }
+*/
+
+//function inside an function
+function getno(a, b)
+{
+function addno ()
+{
+sum = a + b
+console.log( `The sum of a and b is ${sum}`)
+}
+addno()
+}
+getno(12,55)
+
