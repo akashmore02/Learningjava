7b011db76f1cd8f0d6c171f5524181c28f4f138b
akashmore02 <156194217+akashmore02@users.noreply.github.com>
2024-03-15 17:03:32 +0000

    done

--- Iterations/seven.js
+++ Iterations/seven.js
@@ -8,3 +8,10 @@ return acc+cur
 },0)
 
 console.log(myNum);
+
+
+
+//using multiple maps and filter
+let newnumbers = [1,2,3,4,5,6,7,8,9,10]
+let newarr = mynumbers.filter((num)=>num>5).filter((num)=> num>7)
+console.log(newarr);
